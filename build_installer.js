4ce63546eff2506f2d2df88a4fd7f4f1e1542975
fredy122 <i1413470@continental.edu.pe>
2022-06-14 16:50:27 -0500

    cambios en git

---
+++ build_installer.js
@@ -0,0 +1,45 @@
+// ./build_installer.js
+
+// 1. Import Modules
+const { MSICreator } = require('electron-wix-msi');
+const path = require('path');
+
+// 2. Define input and output directory.
+// Important: the directories must be absolute, not relative e.g
+// appDirectory: "C:\\Users\sdkca\Desktop\OurCodeWorld-win32-x64", 
+const APP_DIR = path.resolve(__dirname, './eC34-Pedidos-win32-x64');
+// outputDirectory: "C:\\Users\sdkca\Desktop\windows_installer", 
+const OUT_DIR = path.resolve(__dirname, './windows_installer');
+const LOGO = path.resolve(__dirname, './logo.ico');
+const BACKGROUND = path.resolve(__dirname, './background.jpg');
+
+// 3. Instantiate the MSICreator
+const msiCreator = new MSICreator({
+    appDirectory: APP_DIR,
+    outputDirectory: OUT_DIR,
+
+    // Configure metadata
+    description: 'Sistema de Restaurantes',
+    exe: 'eC34-Pedidos',
+    name: 'eC34-Pedidos',
+    manufacturer: 'Sistemas C34',
+    version: '1.0.0',
+    cultures: 'es-es',
+    language: 1034,
+    appIconPath: LOGO,
+
+    // Configure installer User Interface
+    ui: {
+        chooseDirectory: true,
+        images: {
+            background: BACKGROUND,
+        },
+    },
+});
+
+// 4. Create a .wxs template file
+msiCreator.create().then(function(){
+
+    // Step 5: Compile the template to a .msi file
+    msiCreator.compile();
+});
